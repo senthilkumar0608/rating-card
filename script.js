f32b853faed7ad7acfa3b4892287fc94e8015dc4
senthilkumar0608 <senthilkumar06082003@gmail.com>
2024-05-25 21:03:48 +0530

    script

---
+++ script.js
@@ -0,0 +1,5 @@
+'use script';
+
+const card=document.querySelector([data-card]);
+const submitbtn=doument.querySelector(["data-submit-btn"])
+const ratingbtn=doument.queryselector(["data-rating-btn"])
