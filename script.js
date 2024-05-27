05be1c7fb4743860ba5404751ed319e086f88ea2
senthilkumar0608 <senthilkumar06082003@gmail.com>
2024-05-27 23:20:31 +0530

    script upd

--- script.js
+++ script.js
@@ -3,3 +3,11 @@
 const card=document.querySelector([data-card]);
 const submitbtn=doument.querySelector(["data-submit-btn"])
 const ratingbtn=doument.queryselector(["data-rating-btn"])
+
+let lastRatedVaIue
+or (let i = @ , ten
+= NaN;
+= ratingBtns.Iength; i < ten; i++) {
+ratingBtns[i] . addEventListenerC"ctick", function ( ) {
+if (lastRatedValue) ratingBtns ClastRatedVaIue
+-J
